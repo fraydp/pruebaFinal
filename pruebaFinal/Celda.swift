2d2851c5ee4e561cea79253b6ecb470b7701a542
Fray Pineda <fraypineda@imac-de-fray.local>
2018-10-13 10:30:56 -0400

    primer comité con tabla creada

---
+++ pruebaFinal/Celda.swift
@@ -0,0 +1,27 @@
+//
+//  Celda.swift
+//  pruebaFinal
+//
+//  Created by Fray Pineda on 10/12/18.
+//  Copyright © 2018 Fray Pineda. All rights reserved.
+//
+
+import UIKit
+
+class Celda: UITableViewCell {
+    
+    @IBOutlet weak var viewCell: UIView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        viewCell.layer.cornerRadius = 6.0
+        viewCell.layer.shadowOpacity = 0.7
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ pruebaFinal/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  pruebaFinal
+//
+//  Created by Fray Pineda on 10/12/18.
+//  Copyright © 2018 Fray Pineda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUp()
+        
+    }
+
+    func setUp(){
+        tableView.delegate = self
+        tableView.dataSource = self
+        let nib = UINib(nibName: "Celda", bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: "Celda")
+    }
+  
+
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Celda") as! Celda
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150.0
+    }
+}
+
